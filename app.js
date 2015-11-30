b49b7c6f2a39e0c33659e9586a06f771d68c40ed
Neil Merchant <phozee@gmail.com>
2015-11-29 19:52:35 -0500

    Port should not be hardcoded

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var moment = require('moment');
 
-var port = 3000;
+var port = process.env && process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/'));
 
